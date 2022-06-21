28ffbae4e09c8fd835cf36f382e2f11a839c3908
Alaicev <altarev123456@mail.ru>
2022-06-21 22:46:12 +0400

    create components

---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const ContentBlock = () => {
+    return (
+        <div className="content">
+          <img src="https://interier-foto.ru/wp-content/uploads/dlinnye-fotografii.jpg" />
+      </div>
+    )
+}
+
+export default ContentBlock
