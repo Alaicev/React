fea4a80fbd910d8a0e69f8e2892e6327e218eebd
Alaicev <altarev123456@mail.ru>
2022-07-16 14:40:26 +0400

    axios и get запросы

--- src/redux/Users-reducer.js
+++ src/redux/Users-reducer.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const FOLLOW = "FOLLOW"
 const UNFOLLOW = "UNFOLLOW"
 const SET_USERS = "SET_USERS"
@@ -5,20 +7,42 @@ const SET_USERS = "SET_USERS"
 
 let initialStore = {
     users: [
-        {id: 1, photo:"https://www.meme-arsenal.com/memes/639a19b1249a4f1d9df7baf2c7bbfb5b.jpg", userName: "Dmitriy", follower: true, cite: "i am a boss", location: {city: "Saratov", country: "Russia"}},
-        {id: 2, photo:"http://sun9-48.userapi.com/impf/aQSkgtn1Fem9J4OAV2n38_8JEvP7jESLek6yog/KDzceDEnCQQ.jpg?size=604x453&quality=96&sign=4bae3199771b7cf8a608d6778c0a7068&type=album", userName: "Roman", follower: false, cite: "vr ar", location: {city: "Saratov", country: "Russia"}},
-        {id: 3, photo:"https://madhunter.ru/wp-content/uploads/2019/11/SHakal-870x400.jpg",userName: "Andrey", follower: false, cite: "llolo", location: {city: "Saratov", country: "Russia"}}
+        // {
+        //     id: 1,
+        //     photo: "https://www.meme-arsenal.com/memes/639a19b1249a4f1d9df7baf2c7bbfb5b.jpg",
+        //     userName: "Dmitriy",
+        //     follower: true,
+        //     cite: "i am a boss",
+        //     location: {city: "Saratov", country: "Russia"}
+        // },
+        // {
+        //     id: 2,
+        //     photo: "http://sun9-48.userapi.com/impf/aQSkgtn1Fem9J4OAV2n38_8JEvP7jESLek6yog/KDzceDEnCQQ.jpg?size=604x453&quality=96&sign=4bae3199771b7cf8a608d6778c0a7068&type=album",
+        //     userName: "Roman",
+        //     follower: false,
+        //     cite: "vr ar",
+        //     location: {city: "Saratov", country: "Russia"}
+        // },
+        // {
+        //     id: 3,
+        //     photo: "https://madhunter.ru/wp-content/uploads/2019/11/SHakal-870x400.jpg",
+        //     userName: "Andrey",
+        //     follower: false,
+        //     cite: "llolo",
+        //     location: {city: "Saratov", country: "Russia"}
+        // }
     ],
     text: "text"
 }
+
 
 const usersReducer = (state = initialStore, action) => {
     // eslint-disable-next-line default-case
     switch (action.type) {
         case FOLLOW:
-            return  {
+            return {
                 ...state,
-                users:state.users.map( u => {
+                users: state.users.map(u => {
                     if (u.id === action.userID) {
                         return {...u, follower: true}
                     }
@@ -26,9 +50,9 @@ const usersReducer = (state = initialStore, action) => {
                 })
             }
         case UNFOLLOW:
-            return  {
+            return {
                 ...state,
-                users:state.users.map( u => {
+                users: state.users.map(u => {
                     if (u.id === action.userID) {
                         return {...u, follower: false}
                     }
@@ -36,7 +60,7 @@ const usersReducer = (state = initialStore, action) => {
                 })
             }
         case SET_USERS:
-            return {...state, users:[...state.users, ...action.users]}
+            return {...state, users: [...state.users, ...action.users]}
     }
     return state
 }
@@ -52,8 +76,8 @@ export const unfollowAC = (userID) => {
     }
 }
 export const setUsersAC = (users) => {
-    return{
-        type:SET_USERS, users
+    return {
+        type: SET_USERS, users
     }
 }
 
