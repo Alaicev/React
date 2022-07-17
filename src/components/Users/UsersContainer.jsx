76850fb657d3dee8c15524d1b027401e9eecb7d5
Alaicev <altarev123456@mail.ru>
2022-07-17 18:13:16 +0400

    axios и get запросы 2 чистая компонента(важно)

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 import {connect} from "react-redux";
-import UsersAPIComponent from "./UsersAPIComponent";
 import {currentPageAC, followAC, setUsersAC, totalUsersAC, unfollowAC} from "../../redux/Users-reducer";
+import axios from "axios";
+import AllUsers from "./AllUsers";
+
+class UsersAPIComponent extends React.Component {
+
+    componentDidMount() {
+        axios
+            .get(`https://social-network.samuraijs.com/API/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(data => {
+                this.props.setUsers(data.data.items)
+                this.props.totalUsersSet(data.data.totalCount)
+            })
+
+    }
+
+    clickPage = (u) => {
+        this.props.currentPageSet(u)
+        axios
+            .get(`https://social-network.samuraijs.com/API/1.0/users?page=${u}&count=${this.props.pageSize}`)
+            .then(data => (this.props.setUsers(data.data.items)))
+
+    }
+
+
+    render() {
+        return <AllUsers currentPage={this.props.currentPage}
+                         pageSize={this.props.pageSize}
+                         totalUserCount={this.props.totalUserCount}
+                         users={this.props.users}
+                         clickPage={this.clickPage}
+                         follow={this.props.follow}
+                         unfollow={this.props.unfollow}
+        />
+
+    }
+}
+
 
 let mapStateToProps =(state) =>{
     return {
